dc4e430278f468474dcf3a80403f238087a377d1
vinxenx0 <vinxenxo@protonmail.com>
2023-11-26 00:36:43 +0100

    services controllers update

--- app/controllers/services_controller.py
+++ app/controllers/services_controller.py
@@ -20,6 +20,9 @@ def services():
 @app.route('/toggle_service/<service_name>', methods=['POST'])
 @login_required
 def toggle_service(service_name):
+    return jsonify(toggle_service_status(service_name))
+
+def toggle_service_status(service_name):
     global SERVICE_STATES
 
     if service_name in SERVICE_STATES:
@@ -27,14 +30,12 @@ def toggle_service(service_name):
 
         if current_state == 'active':
             SERVICE_STATES[service_name] = 'inactive'
-            stop_service(service_name)
+            return stop_service(service_name)
         else:
             SERVICE_STATES[service_name] = 'active'
-            start_service(service_name)
-
-        return jsonify({'status': 'success', 'state': SERVICE_STATES[service_name]})
+            return start_service(service_name)
     else:
-        return jsonify({'status': 'error', 'message': 'Unknown Service'})
+        return {'status': 'error', 'message': 'Unknown Service'}
 
 def check_service_status(service_name):
     # Use system commands to check the status of the services
@@ -68,26 +69,96 @@ def execute_command(command):
         return 'Stopped'  # Return a string indicating the service is stopped
 
 
+def start_web_server():
+    # Logic to start the web server
+    pass
 
+def stop_web_server():
+    # Logic to stop the web server
+    pass 
+
+def start_dns_resolver():
+    # Logic to start the DNS resolver
+    pass
+
+def stop_dns_resolver():
+    # Logic to stop the DNS resolver
+    pass
+
+def start_email_service():
+    # Logic to start the email service
+    pass
+
+def stop_email_service():
+    # Logic to stop the email service
+    pass
+
+def start_task_scheduler():
+    # Logic to start the task scheduler
+    pass
+
+def stop_task_scheduler():
+    # Logic to stop the task scheduler
+    pass
+
+def start_notification_service():
+    # Logic to start the notification service
+    pass
+
+def stop_notification_service():
+    # Logic to stop the notification service
+    pass
+
+def start_file_processor():
+    # Logic to start the file processor
+    pass
+
+def stop_file_processor():
+    # Logic to stop the file processor
+    pass
+
+def start_database_cleaner():
+    # Logic to start the database cleaner
+    pass
+
+def stop_database_cleaner():
+    # Logic to stop the database cleaner
+    pass
 
 def start_service(service_name):
-    # Add logic to start the specified service
-    # You might need to adjust this based on your system configuration
-    pass
+    if service_name == 'web_server':
+        start_web_server()
+    elif service_name == 'dns_resolver':
+        start_dns_resolver()
+    elif service_name == 'email_service':
+        start_email_service()
+    elif service_name == 'task_scheduler':
+        start_task_scheduler()
+    elif service_name == 'notification_service':
+        start_notification_service()
+    elif service_name == 'file_processor':
+        start_file_processor()
+    elif service_name == 'database_cleaner':
+        start_database_cleaner()
+    # Add more services as needed
+
+    return {'status': 'success', 'state': 'Running'}
 
 def stop_service(service_name):
-    # Add logic to stop the specified service
-    # You might need to adjust this based on your system configuration
-    pass
-
-
-def check_service_status(service_name):
-    # Use system commands to check the status of the services
-    # You might need to adjust these commands based on your system configuration
     if service_name == 'web_server':
-        return check_web_server_status()
+        stop_web_server()
     elif service_name == 'dns_resolver':
-        return check_dns_resolver_status()
-    else:
-        return 'Unknown Service'
+        stop_dns_resolver()
+    elif service_name == 'email_service':
+        stop_email_service()
+    elif service_name == 'task_scheduler':
+        stop_task_scheduler()
+    elif service_name == 'notification_service':
+        stop_notification_service()
+    elif service_name == 'file_processor':
+        stop_file_processor()
+    elif service_name == 'database_cleaner':
+        stop_database_cleaner()
+    # Add more services as needed
 
+    return {'status': 'success', 'state': 'Stopped'}
