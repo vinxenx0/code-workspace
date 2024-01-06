c30a5f22275305cb2dcf1de9da90dc3ca435265c
vinxenx0 <vinxenxo@protonmail.com>
2024-01-07 00:26:02 +0100

    out secrets

--- config.py
+++ config.py
@@ -9,7 +9,10 @@ SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:dldlt741@81.19.160.10/int
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+IDS_ESCANEO = ['4b99956ba942f7986ccc2e5c992c3a2a111385bfdbbfa2223818c6a8d9e28510',
+        '41038960d6084d0e2ba5416c0c2a52777cc40e119b7c69fb0aeaa4b8231cd2e0',
+        '5b485f2d386e81e56d67e6f1663d7d965f69985e11d771e56b0caf6f5ecb0849'
+           ]  # Reemplaza con los IDs específicos que se proporcionarán
 
 SECRET_KEY = 'your_secret_key_here'
 
